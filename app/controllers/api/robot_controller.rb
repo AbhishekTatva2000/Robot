3b1657758a9eef5a8ddfc0fa84d2e52207866409
kishan.jivani@tatvasoft.com <kishan.jivani@tatvasoft.com>
2022-04-20 07:24:40 +0000

    Toy Robot app

---
+++ app/controllers/api/robot_controller.rb
@@ -0,0 +1,56 @@
+class Api::RobotController < ApplicationController
+	def orders
+		place = params[:commands][0].split(" ")
+		position = place[1].split(",")
+		x = position[0].strip.to_i
+		y = position[1].strip.to_i
+		direction = position[2]
+		move_shifts = params[:commands].drop(1)
+
+		if x < 0 || y < 0 || x > 4 || y > 4
+			return render json: {messsage: "Invalid starting position."}
+		end
+		move_shifts.each do |cmd|
+			case direction
+			when "NORTH"
+				case cmd
+				when "MOVE"
+					y = y + 1 unless (y+1 < 0) || (y+1 > 4)
+				when "LEFT"
+					direction = "WEST"
+				when "RIGHT"
+					direction = "EAST"
+				end	
+			when "SOUTH"
+				case cmd
+				when "MOVE"
+					y = y - 1  unless (y-1 < 0) || (y-1 > 4)
+				when "LEFT"
+					direction = "EAST"
+				when "RIGHT"
+					direction = "WEST"
+				end										
+			when "EAST"
+				case cmd
+				when "MOVE"
+					x = x + 1  unless (x+1 < 0) || (x+1 > 4)
+				when "LEFT"
+					direction = "NORTH"
+				when "RIGHT"
+					direction = "SOUTH"
+				end	
+			when "WEST"
+				case cmd
+				when "MOVE"
+					x = x - 1  unless (x-1 < 0) || (x-1 > 4)
+				when "LEFT"
+					direction = "SOUTH"
+				when "RIGHT"
+					direction = "NORTH"
+				end			
+			end
+		end
+
+		render json: {location: [x,y,direction]}
+	end
+end
